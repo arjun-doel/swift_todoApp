3da75a63b64e076ca7d986625e1f1bf4f6799c72
Arjun <arjundoel@gmail.com>
2022-01-12 20:12:38 +0000

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Arjun Doel on 12/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
