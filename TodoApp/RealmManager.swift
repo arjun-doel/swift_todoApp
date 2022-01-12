aa8f9d140a9ca45d05bd953cdba8579b988ac1f8
Arjun <arjundoel@gmail.com>
2022-01-12 21:13:52 +0000

    installed realm

---
+++ TodoApp/RealmManager.swift
@@ -0,0 +1,13 @@
+//
+//  RealmManager.swift
+//  TodoApp
+//
+//  Created by Arjun Doel on 12/01/2022.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager: ObservableObject {
+    private(set) var localRealm: Realm?
+}
